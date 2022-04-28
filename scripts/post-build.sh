16352c55621f9f0b003f71a505977675b95c4e4b
Martin Böh <contact@martb.dev>
2022-04-28 20:28:15 +0200

    NetworkManager: move system-connections to persistent data partition

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -8,6 +8,13 @@ BOARD_NAME="$(basename ${BOARD_DIR})"
 . "${BR2_EXTERNAL_SATOS_PATH}/meta"
 . "${BOARD_DIR}/meta"
 
+# /data can be implemented differently but is fixed, so we can assume it exists!
+# Hardlink the main NetworkManager system-connections directory to the persistent "data" folder
+# Make sure device_table.txt has the proper 0600 permissions for all files in /data/system-connections set!
+if [ -e ${TARGET_DIR}/etc/fstab ]; then
+        echo "/data/system-connections /etc/NetworkManager/system-connections none bind 0 0" >> ${TARGET_DIR}/etc/fstab
+fi
+
 . "${SCRIPT_DIR}/rauc.sh"
 
 # Run the required RAUC tasks
