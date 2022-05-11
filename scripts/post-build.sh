56720ea9c92b100deab76c835f045c9ee39183a4
Eric Jedermann <jedermann@cs.uni-kl.de>
2022-05-11 19:48:10 +0200

    add automount of USB-Stick "discosat-config"

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -13,6 +13,7 @@ BOARD_NAME="$(basename ${BOARD_DIR})"
 # Make sure device_table.txt has the proper 0600 permissions for all files in /data/system-connections set!
 if [ -e ${TARGET_DIR}/etc/fstab ]; then
         echo "/data/system-connections /etc/NetworkManager/system-connections none bind 0 0" >> ${TARGET_DIR}/etc/fstab
+        echo "LABEL=discosat-config /data/discosat-config ext4 defaults,nofail 0 0" >> ${TARGET_DIR}/etc/fstab
 fi
 
 . "${SCRIPT_DIR}/rauc.sh"
