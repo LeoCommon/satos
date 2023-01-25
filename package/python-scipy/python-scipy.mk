2d1f9fee5b46741446f597e3bf8acfa2c99157b1
Martin Böh <contact@martb.dev>
2023-01-25 17:14:59 +0100

    package/python-scipy: bump to 1.10.0
    Based on:
    - https://patchwork.ozlabs.org/project/buildroot/patch/20221002082651.29673-1-guillaume.bressaix@gmail.com/
    - https://patchwork.ozlabs.org/project/buildroot/patch/20220727192948.18653-4-guillaume.bressaix@gmail.com/
    
    Co-authored-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

--- package/python-scipy/python-scipy.mk
+++ package/python-scipy/python-scipy.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #
-# python-numpy
+# python-scipy
 #
 ################################################################################
 
-PYTHON_SCIPY_VERSION = 1.8.0
+PYTHON_SCIPY_VERSION = 1.10.0
 PYTHON_SCIPY_SOURCE = scipy-$(PYTHON_SCIPY_VERSION).tar.gz
 PYTHON_SCIPY_SITE = https://github.com/scipy/scipy/releases/download/v$(PYTHON_SCIPY_VERSION)
 PYTHON_SCIPY_LICENSE = \
@@ -14,7 +14,6 @@ PYTHON_SCIPY_LICENSE = \
 	BSD-Style, \
 	Apache-2.0, \
 	MIT
-
 PYTHON_SCIPY_LICENSE_FILES = \
 	LICENSE.txt \
 	scipy/linalg/src/lapack_deprecations/LICENSE \
@@ -23,28 +22,51 @@ PYTHON_SCIPY_LICENSE_FILES = \
 	scipy/sparse/linalg/dsolve/SuperLU/License.txt \
 	scipy/sparse/linalg/eigen/arpack/ARPACK/COPYING \
 	scipy/spatial/qhull_src/COPYING.txt
-
+PYTHON_SCIPY_CPE_ID_VENDOR = scipy
+PYTHON_SCIPY_CPE_ID_PRODUCT = scipy
 PYTHON_SCIPY_DEPENDENCIES += \
 	host-meson \
 	host-python-numpy \
 	host-python-pythran \
+	zlib \
 	lapack \
 	openblas \
-	python-numpy
-
+	python-numpy \
+	python-pybind
 PYTHON_SCIPY_INSTALL_STAGING = YES
+
 PYTHON_SCIPY_SETUP_TYPE = setuptools
+PYTHON_SCIPY_BUILD_OPTS = config_fc --fcompiler=gnu95
+
+PYTHON_SCIPY_CFLAGS = \
+	-I$(STAGING_DIR)/usr/include/python$(PYTHON3_VERSION_MAJOR)
+PYTHON_SCIPY_LDFLAGS = $(TARGET_LDFLAGS) -shared \
+	-L$(PYTHON3_PATH)/site-packages/numpy/core/lib
+
+# -lnpymath npymath.ini localization
+PYTHON_SCIPY_NPY_PKG_CONFIG_PATH = \
+	$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/core/lib/npy-pkg-config
+
+# -lnpyrandom localization
+PYTHON_SCIPY_LDFLAGS += \
+	-L$(STAGING_DIR)/usr/lib/python$(PYTHON3_VERSION_MAJOR)/site-packages/numpy/random/lib
+
+# scipy can use C++11 atomics when available, so we need to link with
+# libatomic for the architectures that need libatomic.
+ifeq ($(BR2_TOOLCHAIN_HAS_LIBATOMIC),y)
+PYTHON_SCIPY_LDFLAGS += -latomic
+endif
 
 PYTHON_SCIPY_ENV = \
 	F90=$(TARGET_FC) \
-	CFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="$(TARGET_LDFLAGS) \
-		-L$(PYTHON3_PATH)/site-packages/numpy/core/lib \
-		-L$(PYTHON3_PATH)/site-packages/numpy/random/lib" 
+	CFLAGS="$(PYTHON_SCIPY_CFLAGS)" \
+	NPY_PKG_CONFIG_PATH=$(PYTHON_SCIPY_NPY_PKG_CONFIG_PATH) \
+	LDFLAGS="$(PYTHON_SCIPY_LDFLAGS)"
 
-# trick to locate 'lapack' and 'blas'
+# Provide system configuration options to numpy distutils extenions, telling
+# to find all include files and libraries in staging directory.
 define PYTHON_SCIPY_CONFIGURE_CMDS
-	rm -f $(@D)/site.cfg
+	-rm -f $(@D)/site.cfg
 	echo "[DEFAULT]" >> $(@D)/site.cfg
 	echo "library_dirs = $(STAGING_DIR)/usr/lib" >> $(@D)/site.cfg
 	echo "include_dirs = $(STAGING_DIR)/usr/include" >> $(@D)/site.cfg
