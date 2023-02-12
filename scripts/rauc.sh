bc0b8c4f410219cb27a4bcebbd86d6b01f080931
Martin Böh <contact@martb.dev>
2023-02-13 00:48:08 +0100

    tooling/rauc: add symlink for latest ota package

--- scripts/rauc.sh
+++ scripts/rauc.sh
@@ -85,6 +85,9 @@ EOF
 
     # Generate rauc bundle for the ota
     ${HOST_DIR}/bin/rauc bundle ${RAUC_PKI_OPTIONS} ${BINARIES_DIR}/temp-fullfs/ ${FULLFS_PATH}
+
+    # Symlink latest ota for board
+    ln -L ${ROOTFS_PATH} ${BINARIES_DIR}/satos-${BOARD_NAME}-latest-ota.raucb
 }
 
 function rauc_copy_keyring {
