23da2b68bab6e52f0b01eaa8e96cccacade37263
Martin Böh <contact@martb.dev>
2023-02-21 20:33:38 +0100

    gr-iridium: remove extra copy, only apply required patch

--- package/gr-iridium/gr-iridium.mk
+++ package/gr-iridium/gr-iridium.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-GR_IRIDIUM_VERSION = 2022-02-02_maint-3.9-modified
-GR_IRIDIUM_SITE = $(BR2_EXTERNAL_SATOS_PATH)/src/gr-iridium
-GR_IRIDIUM_SITE_METHOD = local
+GR_IRIDIUM_VERSION = 0c9defb657895c4b131ff6d6ecf7dd54b200ef6e
+GR_IRIDIUM_SITE = $(call github,muccc,gr-iridium,$(GR_IRIDIUM_VERSION))
+
 GR_IRIDIUM_LICENSE = GPL-3.0+
 
 # Theres no real license file, but we include the readme for now <MartB>
