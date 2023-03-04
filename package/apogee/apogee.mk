e2b6259abd0f787535d447acb76c9ce47bdf9b5b
Martin Böh <contact@martb.dev>
2023-03-04 16:43:20 +0100

    restructure apogee build

--- package/apogee/apogee.mk
+++ package/apogee/apogee.mk
@@ -4,32 +4,32 @@
 #
 ################################################################################
 
-DISCO_APOGEE_VERSION = 0.3
-DISCO_APOGEE_SITE = $(BR2_EXTERNAL_SATOS_PATH)/src/apogee
-DISCO_APOGEE_SITE_METHOD = local
-DISCO_APOGEE_SITE_LICENSE = Proprietary
-DISCO_APOGEE_BUILD_TARGETS = cmd/modem_manager cmd/apogee-client
-DISCO_APOGEE_INSTALL_BINS = modem_manager apogee-client
+APOGEE_VERSION = 0.3
+APOGEE_SITE = $(BR2_EXTERNAL_SATOS_PATH)/src/apogee
+APOGEE_SITE_METHOD = local
+APOGEE_SITE_LICENSE = Proprietary
+APOGEE_BUILD_TARGETS = cmd/modem_manager cmd/client
+APOGEE_INSTALL_BINS = modem_manager client
 
 # Make sure this matches our go.mod file
 # Required because auto-discovery is using URLs within buildroot
-DISCO_APOGEE_GOMOD = "disco.cs.uni-kl.de/apogee"
+APOGEE_GOMOD = "disco.cs.uni-kl.de/apogee"
 
 # Create the required user
 # This also adds the apogee user to the plugdev group so we can use the hackrf
 # Furthermore the systemd-journal group is required to access the journal
-define DISCO_APOGEE_USERS
+define APOGEE_USERS
 	apogee -1 apogee -1 * - - plugdev,systemd-journal Apogee daemon user
 endef
 
 # Install the systemd-service and the required policy kit rules
-define DISCO_APOGEE_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 644 $(DISCO_APOGEE_PKGDIR)/apogee-client.service \
-		$(TARGET_DIR)/usr/lib/systemd/system/apogee-client.service
+define APOGEE_INSTALL_INIT_SYSTEMD
+	$(INSTALL) -D -m 644 $(@D)/dist/systemd/client.service \
+		$(TARGET_DIR)/usr/lib/systemd/system/client.service
 
 	mkdir -p $(TARGET_DIR)/etc/polkit-1/rules.d
-	$(INSTALL) -D -m 644 $(DISCO_APOGEE_PKGDIR)disco-apogee.rules \
-		$(TARGET_DIR)/etc/polkit-1/rules.d/10-disco-apogee.rules
+	$(INSTALL) -D -m 644 $(@D)/dist/polkit/client.rules \
+		$(TARGET_DIR)/etc/polkit-1/rules.d/10-client.rules
 endef
 
 $(eval $(golang-package))
--- package/rauc-hawkbit-updater/rauc-postinst-hook.sh
+++ package/rauc-hawkbit-updater/rauc-postinst-hook.sh
@@ -2,24 +2,23 @@
 
 # The reboot function delays the reboot with a systemd service
 # This is needed for the update to be marked succesful.
-function reboot()
-{
-   echo "rebooting in 10 seconds now!"
-   systemd-run -q sh -c 'sleep 10 && reboot'
+reboot() {
+    echo "rebooting in 10 seconds now!"
+    systemd-run -q sh -c 'sleep 10 && reboot'
 }
 
 # Send SIGUSR1 signal to the process to signal a finished update
-echo 'Marking reboot and sending SIGUSR1 to apogee-client'
+echo 'Marking reboot and sending SIGUSR1 to client'
 
 # Create a magic file so we dont miss the request if apogee restarts.
 touch "/tmp/.reboot-pending"
 chown apogee:apogee /tmp/.reboot-pending
 
-if systemctl kill -s SIGUSR1 --kill-who=main apogee-client 2>/dev/null; then
+if systemctl kill -s SIGUSR1 --kill-who=main client 2>/dev/null; then
     echo "Signal delivered to apogee, terminating script"
     exit 0
 fi
 
 echo 'Apogee daemon not running, trigger reboot'
-reboot 
+reboot
 exit 0
